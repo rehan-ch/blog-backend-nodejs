b360318a7d7d82d286a0aa2639030d0b2e823701
Muhammad Rehan <rehanch642@gmail.com>
2024-09-22 19:22:51 +0500

    routes updated for user crud

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,10 @@
 import { Router } from "express";
-import { getAllUsers } from "../controllers/userController.js";
+import { createUser, deleteUserById, getAllUsers, getUserById, updateUserById } from "../controllers/userController.js";
 
 const router = Router();
 router.get('/', getAllUsers);
+router.get('/:id', getUserById);
+router.post('/', createUser);
+router.put('/:id', updateUserById);
+router.delete('/:id', deleteUserById)
 export default router;
